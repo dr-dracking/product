eb66bb8eac06b68a9407373470f5fc9d19075f62
glasdou.dev <dr.glasdou@gmail.com>
2024-07-09 11:18:45 -0600

    feat: Add methods for safe deletion and key exclusion in ObjectManipulator helper

--- src/helpers/object-manipulator.helper.ts
+++ src/helpers/object-manipulator.helper.ts
@@ -1,4 +1,10 @@
 export class ObjectManipulator {
+  /**
+   * Safely delete a key from an object
+   * @param obj The object to be manipulated
+   * @param key The key to be deleted
+   * @returns void
+   */
   static safeDelete<T extends object, K extends keyof T>(obj: T, key: K): void {
     if (!(key in obj)) {
       console.warn(`Property ${String(key)} does not exist on the object.`);
@@ -8,6 +14,12 @@ export class ObjectManipulator {
     delete obj[key];
   }
 
+  /**
+   * Exclude specified keys from an object
+   * @param obj The object to be manipulated
+   * @param keys The keys to be deleted
+   * @returns The object without the specified keys
+   */
   static exclude<T extends object, K extends keyof T>(obj: T, keys: K[]): Partial<T> {
     return Object.keys(obj).reduce((acc, key) => {
       if (!keys.includes(key as K)) {
